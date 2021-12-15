a8f3f2f1f98f07b646dccfcc9d8a81b720980306
d33zhou <d33zhou@outlook.com>
2021-12-15 07:53:15 +0000

    update counter logic to turn remaining count red when negative and back to normal when 0 or above

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,19 @@
 // to modify character counter when composing new tweets
 
 $(document).ready(function() {
+  // update remaining character count on input
   $('#tweet-text').on('input', function() {
     const charCount = this.value.length; // modify to bind this to use jquery built-in .val()
+    const remaining = 140 - charCount;
+
+    $('.counter').val(remaining); // should not use the .counter class, use DOM traversal instead
     
-    $('.counter').val(140 - charCount);
-    
+    if (remaining >= 0) {
+      $('.counter').attr("sign", "positive");
+    } else {
+      $('.counter').attr("sign", "negative");
+    }
+
     // console.log(140 - charCount);
-  })
+  });
 });
