9a086cf3808e632957c261216719543aa5d9d862
d33zhou <d33zhou@outlook.com>
2021-12-15 23:06:05 +0000

    add dynamic new tweet rendering behaviour

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,3 +3,48 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+$(document).ready(function() {
+
+  const createTweetElement = function(tweetData) {
+    return $(`<article class="tweet">
+      <header>
+        <p class="pic-name"><img src=${tweetData.user.avatars}> ${tweetData.user.name}</p>
+        <p class="handle">${tweetData.user.handle}</p>
+      </header>
+      <div class="content">
+        <p>${tweetData.content.text}</p>
+      </div>
+      <footer>
+        <p>${tweetData.created_at}</p>
+        <div class="icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+    <br>`);
+  }
+
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  }
+
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+});
