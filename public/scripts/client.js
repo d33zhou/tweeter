2d3d0e34dd84a8a9904de5883501dc8d871654d5
d33zhou <d33zhou@outlook.com>
2021-12-16 06:09:56 +0000

    update validation errors formatting and display logic

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,37 +6,45 @@
 
 $(document).ready(function() {
 
+  $('.error').hide(0);
+
   // handler when new tweets submitted
   $('#submit-tweet').submit(function(event) {
     event.preventDefault();
-    
-    // data validation for submitted tweet - no empty tweets or >140 char tweets
-    const newTweet = $('#tweet-text').val();
-    if (newTweet === "" || newTweet === null) {
-      alert("Error: You have nothing to Tweet!");
-    } else if (newTweet.length > 140) {
-      alert("Error: You Tweeted too much!");
-    } else {
-      // submit POST request with serialized data (query string format)
-      $.post("/tweets", $(this).serialize())
-        .done(function() {
-          
-          // add the newest tweet card to the top of the tweets container
-          $.get("/tweets", function(data) {
-            const recentTweet = data[data.length - 1];
-            const $newTweet = createTweetElement(recentTweet);
-            $('#tweets-container').prepend($newTweet);
+
+    $('.error').slideUp("fast", function() {
+
+      // data validation for submitted tweet - no empty tweets or >140 char tweets
+      const newTweet = $('#tweet-text').val();
+      if (newTweet === "" || newTweet === null) {
+        
+        $('span.error').html("You didn't Tweet anything! Try humming a different tune.");
+        $('.error').slideDown("fast");
+
+      } else if (newTweet.length > 140) {
+        
+        $('span.error').html("You Tweeted too much! Try humming a shorter tune.");
+        $('.error').slideDown("fast");
+
+      } else {
+        // submit POST request with serialized data (query string format)
+        $.post("/tweets", $(this).serialize())
+          .done(function() {
+            
+            // add the newest tweet card to the top of the tweets container
+            $.get("/tweets", function(data) {
+              const recentTweet = data[data.length - 1];
+              const $newTweet = createTweetElement(recentTweet);
+              $('#tweets-container').prepend($newTweet);
+            });
           });
-        });
 
-      // reformat tweeter text box and counter for next tweet
-      $('#tweet-text').val("").focus();
-      $('.counter').val(140);
+        // reformat tweeter text box and counter for next tweet
+        $('#tweet-text').val("").focus();
+        $('.counter').val(140);
 
-      
-      
-      
-    }
+      }
+    });
   });
 
   // GET all tweets data from server database and generate tweet cards
@@ -61,7 +69,7 @@ const createTweetElement = function(tweet) {
       <p class="handle">${tweet.user.handle}</p>
     </header>
     <div class="content">
-      <p>${tweet.content.text}</p>
+      <p>${escapeXSS(tweet.content.text)}</p>
     </div>
     <footer>
       <p>${timeago.format(tweet.created_at)}</p>
@@ -81,4 +89,11 @@ const renderTweets = function(tweets) {
     const $tweet = createTweetElement(tweets[i]);
     $('#tweets-container').append($tweet);
   }
+};
+
+// escape function to prevent cross-site scripting
+const escapeXSS = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
 };
