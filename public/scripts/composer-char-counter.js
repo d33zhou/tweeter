5a2811316161b432babd98e3b49f01aae003f486
d33zhou <d33zhou@outlook.com>
2021-12-17 02:40:44 +0000

    update scss styling for clarity/refactoring and refactor counter.js logic

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,16 @@
-// to modify character counter when composing new tweets
-
+// Modify remaining character count on user input
 $(document).ready(function() {
   // update remaining character count on input
   $('#tweet-text').on('input', function() {
-    const charCount = this.value.length; // modify to bind this to use jquery built-in .val()
-    const remaining = 140 - charCount;
-
-    $('.counter').val(remaining); // should not use the .counter class, use DOM traversal instead
+    const field = $(this);
+    const output = field.parent().find("output");
     
-    $('.counter').attr("sign", remaining >= 0 ? "positive" : "negative");
+    // calc remaining count - 140 chars is set limit
+    const remaining = 140 - field.val().length;
+
+    // update character counter for remaining count and attribute (for color styling)
+    output
+      .val(remaining)
+      .attr("sign", remaining >= 0 ? "positive" : "negative");
   });
 });
